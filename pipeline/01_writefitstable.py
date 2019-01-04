60d96c798671720acb90b6d48422fb7fd11dc687
Nao Suzuki <nsuzuki@master.hsc.ipmu.jp>
2019-01-05 02:41:56 +0900

    NS

--- pipeline/01_writefitstable.py
+++ pipeline/01_writefitstable.py
@@ -2,20 +2,22 @@ import pyfits
 import plasticc
 
 datadir='/Users/suzuki/github/projects_plasticc/data_original/'
+datadir='/work200t/nsuzuki/plasticc/data/'
 
 def write_fitstable_ltcv():
     pl=plasticc.LSSTplasticc()
     #pl.read_ltcvdata('../data/training_set.csv')
     #pl.read_ltcvdata(datadir+'/test_set.csv')
+    pl.read_ltcvdata(datadir+'/training_set.csv')
     #pl.read_ltcvdata_genfromtxt(datadir+'/test_set100.csv')
-    pl.read_ltcvdata_genfromtxt(datadir+'/test_set.csv')
+    #pl.read_ltcvdata_genfromtxt(datadir+'/test_set.csv')
     print('Finished Reading csv, Number of Object=',len(pl.ltcv['object_id']))
 
     c1=pyfits.Column(name='object_id',format='J',array=pl.ltcv['object_id'])
     print('mjd',len(pl.ltcv['mjd']))
     c2=pyfits.Column(name='mjd',format='E',array=pl.ltcv['mjd'])
     c3=pyfits.Column(name='passband',format='I',array=pl.ltcv['passband'])
-    print('flux',len(pl.ltcv['flux']))
+    # print('flux',len(pl.ltcv['flux']))
     c4=pyfits.Column(name='flux',format='E',array=pl.ltcv['flux'])
     c5=pyfits.Column(name='flux_err',format='E',array=pl.ltcv['flux_err'])
     c6=pyfits.Column(name='detected',format='L',array=pl.ltcv['detected'])
@@ -24,6 +26,7 @@ def write_fitstable_ltcv():
     #tblhdu.writeto('training_set.fits')
     #tblhdu.writeto(datadir+'/test_set.fits')
     #tblhdu.writeto('test_set100.fits')
-    tblhdu.writeto('test_set.fits')
+    #tblhdu.writeto(datadir+'test_set.fits')
+    tblhdu.writeto(datadir+'training_set.fits')
 
 write_fitstable_ltcv()
--- pipeline/02_readfits_ltcv.py
+++ pipeline/02_readfits_ltcv.py
@@ -4,6 +4,7 @@ import time
 import sys
 
 datadir='/Users/suzuki/github/projects_plasticc/data_original/'
+datadir='/work200t/nsuzuki/plasticc/data/'
 
 '''
 2018-12-01 : Nao Suzuki
@@ -12,8 +13,14 @@ Reading Plasticc FITS Table
 
 def read_fitstable_ltcv():
     start=time.time()
-    ltcvtbl=pyfits.open('test_set100.fits',memmap=True)
+    #ltcvtbl=pyfits.open('test_set100.fits',memmap=True)
+    #ltcvtbl=pyfits.open('test_set100.fits',memmap=True)
+    ltcvtbl=pyfits.open(datadir+'test_set.fits',memmap=True)
     ltcvdata=ltcvtbl[1].data
-    print(ltcvdata)
+    #rows=numpy.arange(20)
+    #print(ltcvdata[rows])
+    print(ltcvdata[-3])
+    print(ltcvdata[-2])
+    print(ltcvdata[-1])
 
 read_fitstable_ltcv()
